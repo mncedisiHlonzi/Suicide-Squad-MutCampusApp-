0a6a8e1dac19dcc674a924c1c5ed165b7a630967
Thandazani(Mabutho) Mbutho <141316674+Gozilethu@users.noreply.github.com>
2024-07-25 11:32:10 +0200

    Add files via upload

--- MutCampusApp/src/app/navigation/tab2.page.ts
+++ MutCampusApp/src/app/navigation/tab2.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import mapboxgl from 'mapbox-gl';  // Correct way to import with allowSyntheticDefaultImports enabled
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  map: mapboxgl.Map | undefined;
 
-  constructor() {}
+  constructor() {
+    mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+  }
 
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [30.9107, -29.9698],
+      zoom: 18
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat([30.9107, -29.9698])
+      .setPopup(new mapboxgl.Popup().setHTML('<h3>Student Center</h3>'))
+      .addTo(this.map);
+  }
 }
